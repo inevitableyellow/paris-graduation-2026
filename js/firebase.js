c636609095b4b517631a95a3467a5d53581d2908
inevitableyellow <pmheard@umich.edu>
2026-05-05 18:38:51 -0400

    update admin login

--- js/firebase.js
+++ js/firebase.js
@@ -8,6 +8,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC_xOO38k0i4mouvvGc5ZT5DB0Yb14F8xc",
@@ -23,3 +24,4 @@ const app = initializeApp(firebaseConfig);
 
 export const db      = getFirestore(app);
 export const storage = getStorage(app);
+export const auth    = getAuth(app);
